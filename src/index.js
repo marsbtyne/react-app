79b8b4815711f7461c7ea1fe3d3eec95591265a2
Mars Ballantyne <mballantyne@mballantyne-mac.local>
2018-03-06 10:28:26 -0500

    set up the person object

--- src/index.js
+++ src/index.js
@@ -16,6 +16,10 @@ import registerServiceWorker from './registerServiceWorker';
 //   rawFile.send(null);
 // }
 
+let person ={ 
+};
+let personArray=[];
+
 function loadFile(e) {
 	let file = e.target.files[0];
 	if (!file) {
@@ -31,7 +35,29 @@ function loadFile(e) {
 
 function displayContents(contents){
 	let element = document.getElementById('file-content');
-	element.innerHTML = contents;
+	//let allLines = contents.split(/\r\n|n/);
+	let lines = contents.split('\n');
+	let firstLine = lines[0].split(',');
+	let firstName = firstLine[0];
+	let lastName = firstLine[1];
+	let age = firstLine[2];
+	let state = firstLine[3];
+	let people =[];
+
+	for (let personIndex = 1; personIndex < lines.length-1; personIndex++){
+		let line = lines[personIndex];
+		personArray = line.split(',');
+		person[firstName] = personArray[0];
+		people.push(person);
+	}
+	// lines.forEach (function(line){
+	// 	person = line.split(',');
+	// 	people.push(person);
+	// });
+	
+
+	//let firstName = entries
+	element.innerHTML = people[0][firstName];
 }
 
 
