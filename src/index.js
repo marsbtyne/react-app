5ba3036197a989e2e6a434c13cc7633bd11334f4
Mars Ballantyne <mballantyne@mballantyne-mac.local>
2018-03-07 13:57:01 -0500

    found a better way to iterate through states

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ function Person(firstName, lastName, age, state) {
 	this.lastName = lastName;
 	this.age = age;
 	this.state = state;
+	this.isOver30 = (age >= 30)
 }
 
 
@@ -40,24 +41,41 @@ function loadFile(e) {
 
 function createPeople(contents){
 	let people =[];
+	let statesUnder30 = new Map();
+	let statesOver30 = new Map();
 	
 	let lines = contents.split('\n');
 	let firstLine = lines[0].split(',');
-	let person = {
-		firstName: '',
-		lastName: '',
-		age: 0,
-		state: ''
-	}
 
 	for (let personIndex = 1; personIndex < lines.length-1; personIndex++){
 		let line = lines[personIndex];
 		let personArray = line.split(',');
-		let person = new Person(personArray[0], personArray[1], personArray[2], personArray[3]);
-		people.push(person);
-	}
-	return people;
+		let firstName = personArray[0];
+		let lastName =  personArray[1];
+		let age =  personArray[2];
+		let state = personArray[3];
+
+
+
+		let person = new Person(firstName, lastName, age, state);
+		
+		person.isOver30 ? addPersonToState(person, statesOver30) : 
+			addPersonToState(person, statesUnder30)
+		}
+	return [statesUnder30, statesOver30];
 }
+
+function addPersonToState(person, states){
+	if (!(states.has(person.state))){
+		let p = [person];
+		states.set(person.state, p)
+	}
+	else {
+		states.get(person.state).push(person);
+		}
+	return states;
+}
+
 
 // function sortPeople(people) {
 // 	let over30 = people.filter(person => person[age] >= 30);
@@ -66,20 +84,40 @@ function createPeople(contents){
 
 function displayContents(contents){
 	let element = document.getElementById('file-content');
-	let people = createPeople(contents);
-	//element.innerHTML = people[0].firstName;
+	let under30 = createPeople(contents)[0];
+
+	let statePeople = under30.forEach(function(value, key, map){
+		let stateElem = document.createElement(key);
+		stateElem.innerHTML = "State: " + key;
+		element.appendChild(stateElem);
+
+	});
+
+	//element.innerHTML = states.next().value;
 
 	let over30element = document.getElementById('over-30');
 
-	let over30 = people.filter(person => person.age >= 30);
-	let under30 = people.filter(person => person.age < 30);
+	//let over30 = people.filter(person => person.age >= 30);
+	//let under30 = people.filter(person => person.age < 30);
 
-	over30.forEach(function(person){
+	
+
+	//let i = over30States.entries();
 	let elem = document.createElement('div');
-	let p = "Document: " + person.firstName + ",";
-	elem.innerHTML = p;
-	element.appendChild(elem);
-	});
+
+
+	// over30.forEach(function(person){
+
+		
+	// 	//states.indexOf(person.state).push(person);
+	// 	let elem = document.createElement('div');
+	// 	let p = "Document: " + person.firstName + ",";
+	// 	//console.log(over30States);
+	// 	elem.innerHTML = p;
+	// 	element.appendChild(elem);
+	// });
+
+	// var states = over30.map
 }
 
 
